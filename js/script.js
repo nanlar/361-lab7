c271b312b90a08a903ff555a065491647d598c82
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-13 17:41:34 -0500

    Added the javascript code to try.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 function init(){
-//add your javascrip between these two lines of code
+// my javascrip code is below
  
+	function myEventFunction(){
+	  alert("Nilufer Anlar:" + ebutton.value);
+	}
+        
+  var ebutton = document.getElementById('entrybutton');
+  var einp = document.getElementById('entryinput');
+		    
+	ebutton.addEventListener('click', myEventFunction);
+        
+	eb.addEventListener('click', function(){
+          
+  console.log('I was Clicked!');
+          
+  var txtoutp = document.getElementById("textoutput");
+  txtoutp.innerHTML = einp.value;
 
-
-
-
-
-
-
+});
+          
 window.addEventListener('load', init);
