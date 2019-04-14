9bf417f62686088a08aa66464fe7d5924bbd15e2
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-13 19:15:14 -0500

    Updated the JS script, moved both actions inside a function.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function init(){
 // my javascrip code is below
 
+// get the element ids first
   var einp = document.getElementById("entryinput");
   var ebutton = document.getElementById("entrybutton");
   var txtoutp = document.getElementById("textoutput");
-	      
+	
+  // do the requested actions inside a function
 	ebutton.addEventListener("click", function(){
-    alert("Nilufer Anlar:" + einp.value);
+    alert("Nilufer Anlar: " + einp.value);
     console.log("I was Clicked!");
     txtoutp.innerHTML = einp.value;
   });
