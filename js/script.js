c7f53b2e890691ba02ff383d27cd2300246e9121
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-13 20:35:04 -0500

    Re-edited the comments.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function init(){
-// my javascrip code is below
+// my javascript code below uses my function to do two actions
   function myEventFunction(){
     var einp = document.getElementById("entryinput");
     alert("Nilufer Anlar: " + einp.value);
+
     var txtoutp = document.getElementById("textoutput");
     txtoutp.innerHTML = einp.value;
 	}
 
   var ebutton = document.getElementById("entrybutton");
-  // do the requested actions inside my function
 	ebutton.addEventListener("click", myEventFunction);
 }
           
